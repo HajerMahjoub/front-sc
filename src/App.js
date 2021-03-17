a183269025a7c598c25a63966320e2a071898e00
Hajer Mahjoub <hajer.mahjoub@marem.io>
2021-03-17 11:38:51 +0100

    add the scraper

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+import axios from 'axios';
+import qs from 'qs';
 
-function App() {
+const queryClient = new QueryClient()
+
+function Products() {
+  const { isLoading, error, data } = useQuery('getProducts', () =>
+    axios.post('http://localhost:3000/products/related', {
+      url: qs.parse(document.location.search, { ignoreQueryPrefix: true }).url
+    })
+  )
+  console.log({error, data})
+  if (isLoading) return 'Loading';
+  if (error) return null;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        data.data.map(e => {
+          return <div>
+            <h1>
+              <a href={e.url}>{e.title}</a>
+            </h1>
+            <b>
+              {
+                e.prices.join(' - ')
+              }
+            </b>
+            <p>
+              {e.description}
+            </p>
+            <div>
+              {
+                e.images.map(im => {
+                  return <img src={im} />
+                })
+              }
+            </div>
+          </div>
+        })
+      }
     </div>
   );
+}
+
+function App() {
+  return <QueryClientProvider client={queryClient}>
+    <Products />
+  </QueryClientProvider>
 }
 
 export default App;
